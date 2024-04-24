d8b94e91cacfe2ad26df3e36f8ab02926074ad88
EduCamelo <eduardocamelobs@gmail.com>
2024-04-24 15:48:38 -0300

    Projeto finalizado

--- main.cpp
+++ main.cpp
@@ -27,15 +27,15 @@ void drawBezierCurve(float x0, float y0, float x1, float y1, float x2, float y2,
     glBegin(GL_LINE_STRIP);
     for (int i = 0; i <= 100; i++) {
         float t = i / 100.0f;
-        float x = pow(1 - t, 3) * (x0) + 3 * t * pow(1 - t, 2) * (x1) + 3 * pow(t, 2) * (1 - t) * (x2) + pow(t, 3) * (x3);
-        float y = pow(1 - t, 3) * (y0) + 3 * t * pow(1 - t, 2) * (y1) + 3 * pow(t, 2) * (1 - t) * (y2) + pow(t, 3) * (y3);
+        float x = pow(1 - t, 3) * (x0)+3 * t * pow(1 - t, 2) * (x1)+3 * pow(t, 2) * (1 - t) * (x2)+pow(t, 3) * (x3);
+        float y = pow(1 - t, 3) * (y0)+3 * t * pow(1 - t, 2) * (y1)+3 * pow(t, 2) * (1 - t) * (y2)+pow(t, 3) * (y3);
         glVertex2d(x, y);
     }
     glEnd();
 }
 
 //Leitura do arquivo.obj
-bool loadObj(const char* filename, std::vector<Vertex>& vertices, std::vector<int>& transformacao,std::vector<Transforme>& transforme) {
+bool loadObj(std::string filename, std::vector<Vertex>& vertices, std::vector<int>& transformacao, std::vector<Transforme>& transforme) {
     std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Erro ao abrir o arquivo.\n";
@@ -91,7 +91,7 @@ static void teclado(GLFWwindow* window, int key, int scancode, int action, int m
         aux = !aux;
     }
 
-    if(key == GLFW_KEY_A && action == GLFW_PRESS) {
+    if (key == GLFW_KEY_A && action == GLFW_PRESS) {
         aux3 = !aux3;
     }
 
@@ -152,7 +152,7 @@ void desenho2(std::vector<Vertex> vertices) {
 void escala(std::vector<Vertex>& transforme, float escalaX, float escalaY) {
     std::vector<Vertex> auxTransforme(transforme);
     transforme.clear();
-    for ( const auto& point : auxTransforme)
+    for (const auto& point : auxTransforme)
     {
         Vertex pontoTransformado;
         pontoTransformado.x = point.x * escalaX;
@@ -175,10 +175,10 @@ void translacao(std::vector<Vertex>& transforme, float transX, float transY) {
 }
 
 //Logica do rotação, 2d
-void rotacaoPontos(std::vector<Vertex>& transforme,float angle, Vertex centro) {
+void rotacaoPontos(std::vector<Vertex>& transforme, float angle, Vertex centro) {
     std::vector<Vertex> auxTransforme(transforme);
-    transforme.clear(); 
-    
+    transforme.clear();
+
     // Tive que transformar graus em radianos (obs: proximei PI)
     angle = angle * 3.14 / 180;
     float cos = std::cos(angle);
@@ -187,26 +187,26 @@ void rotacaoPontos(std::vector<Vertex>& transforme,float angle, Vertex centro) {
     for (const auto& point : auxTransforme) {
         Vertex pontoTransformado;
 
-        pontoTransformado.x = centro.x+(point.x - centro.x)*cos-(point.y-centro.y)*seno;
-        pontoTransformado.y = centro.y+(point.x - centro.x)* seno + (point.y - centro.y)*cos;
+        pontoTransformado.x = centro.x + (point.x - centro.x) * cos - (point.y - centro.y) * seno;
+        pontoTransformado.y = centro.y + (point.x - centro.x) * seno + (point.y - centro.y) * cos;
         transforme.push_back(pontoTransformado);
     }
 }
 
 //Logica do cisalhamento, 2d
-void cisa(std::vector<Vertex>& transforme,float shearX, float shearY, Vertex centro){
-        std::vector<Vertex> auxTransforme(transforme);
-        transforme.clear();
-        // Aplicar cisalhamento aos pontos originais
-        for (const auto& point : auxTransforme) {
-            Vertex pontoTransformado;
+void cisa(std::vector<Vertex>& transforme, float shearX, float shearY, Vertex centro) {
+    std::vector<Vertex> auxTransforme(transforme);
+    transforme.clear();
+    // Aplicar cisalhamento aos pontos originais
+    for (const auto& point : auxTransforme) {
+        Vertex pontoTransformado;
 
-            pontoTransformado.x = point.x + shearX * (point.y-centro.y);
-            pontoTransformado.y = point.y + shearY * (point.x- centro.x);
+        pontoTransformado.x = point.x + shearX * (point.y - centro.y);
+        pontoTransformado.y = point.y + shearY * (point.x - centro.x);
 
-            transforme.push_back(pontoTransformado);
-        }
-    
+        transforme.push_back(pontoTransformado);
+    }
+
 }
 
 int main(void)
@@ -235,8 +235,7 @@ int main(void)
     }
 
     glfwSetWindowPos(window, 50, 100);
-
-
+    glLineWidth(2);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
@@ -265,6 +264,7 @@ int main(void)
             glVertex2f(0.0f, 240);
             glEnd();
 
+            glLineWidth(2.0f);
             //Pinta o desenho
             if (aux3) {
                 desenho2(anterior);
@@ -294,7 +294,7 @@ int main(void)
                         break;
                     case 3:
                         //Escala
-                        escala(transformado,transforme[pimp].x, transforme[pimp].y);
+                        escala(transformado, transforme[pimp].x, transforme[pimp].y);
                         break;
 
                     case 4:
@@ -316,9 +316,9 @@ int main(void)
                     }
                 }
                 aux2 = false;
-                
+
             }
-                
+
             /* Poll for and process events */
             glfwSwapBuffers(window);
             glfwPollEvents();
